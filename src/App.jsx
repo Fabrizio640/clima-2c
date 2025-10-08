f248c8d58deeee0e297333dc281688fe4883a876
Fabrizio640 <fabriziomatias52@gmail.com>
2025-10-08 12:18:03 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { CloudSun } from 'lucide-react';
+import { CloudSun,  MapPinned } from 'lucide-react';
 
 function App() {
   const [cidade, setCidade] = useState('');
@@ -32,13 +32,43 @@ function App() {
       <div className="container">
         <div className="content">
           <header>
-            <h1> <CloudSun color="white" size={48} />
-            Consulta do Clima</h1>
+            <h1>
+              <CloudSun color="white" size={48} />
+              Consulta de Clima
+            </h1>
             <p>Exemplo de consumo de API com React</p>
           </header>
+          
           <div className="busca-box">
-            <div className="busca-container"><input type="text" placeholder="Digite o nome da cidade..." />
-            <button>Buscar</button></div>
+            <div className="busca-container">
+              <input 
+                type="text"
+                placeholder="Digite o nome da cidade.."
+              />
+            <button>Buscar</button>
+          </div>
+          </div>
+
+          {/* Resultado do clima */}
+          <div id="card-resultado">
+            <div id="cidade-info">
+              <div id="cidade-nome">
+              <MapPinned style={{color: '#550808ff'}} size={48} />
+                Campinas, BR
+              </div>
+              <p id="cidade-desc">
+                Nublado
+              </p>
+            </div>
+
+            {/* Temperatura principapl */}
+            <div id="temperatura-box">
+              <div id="temp-valor">21°C</div>
+              <div id="temp-sens">
+                Sensação Térmica: 21°C
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
